419618e369cf74501652d35f5d6b9e4ba096c072
LuisPaDo <2020-40108@liceocanadiense.edu.gt>
2024-06-06 14:12:48 -0600

    se van a subir 3 proyectos

---
+++ ejemplo.py
@@ -0,0 +1,15 @@
+a=8
+b=7
+c=5
+d=6
+e=4
+g=a+b+c+d+e
+h=a-b-c-d-e
+j=a*b*c*d*e
+k=a/b/c/d/e
+
+print ("la suma es igual a ",g)
+print ("la resta es igual a ",h)
+print ("la multiplicacion es igual a ",j)
+print ("la division es igual a ",k)
+
