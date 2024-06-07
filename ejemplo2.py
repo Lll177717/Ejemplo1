422d15075d15ca18ab7b39f56900eb8c9d1c85f7
LuisPaDo <2020-40108@liceocanadiense.edu.gt>
2024-06-07 12:37:48 -0600

    mensaje para saber los cambios realizados

---
+++ ejemplo2.py
@@ -0,0 +1,16 @@
+n1=int (input ("ingrese su primer dato \n" ))
+n2=int (input ("ingrese su segundo dato \n"))
+n3=int (input ("ingrese su tercer dato \n" ))
+n4=int (input ("ingrese su cuarto dato \n"))
+n5=int (input ("ingrese su quinto dato \n" ))
+n6= n1+n2+n3+n4+n5
+print ("la suma de los datos es " + str (n6))
+
+n7= n1-n2-n3-n4-n5
+print ("la resta de los datos es " + str (n7))
+
+n8= n1*n2*n3*n4*n5
+print ("la multiplicacion de los datos es " + str (n8))
+
+n9= n1/n2/n3/n4/n5
+print ("la division de los datos es " + str (n9))
